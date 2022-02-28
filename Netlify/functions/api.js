983a620b864d61a6042c14c444e870baeb1196fd
QualityPre <qualitypre@protonmail.com>
2022-02-28 11:50:17 +0000

    netlify

--- Netlify/functions/api.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      api: process.env.REACT_APP_API_KEY,
-    }),
-  }
-}
--- src/components/Country.js
+++ src/components/Country.js
@@ -7,12 +7,6 @@ let api_key = process.env.REACT_APP_API_KEY
 const Country = ({ country }) => {
   const [weatherData, setWeatherData] = useState()
 
-  // fetch('.netlify/functions/api')
-  //   .then((response) => response.json())
-  //   .then((json) => {
-  //     api_key = json.api
-  //   })
-
   let capital = country.capital
   const weatherHook = () => {
     axios
