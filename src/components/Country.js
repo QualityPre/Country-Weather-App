73ffb2e22237647bfb60223b5e054642a1e121cd
QualityPre <qualitypre@protonmail.com>
2022-03-16 15:02:47 +0000

    pretty weather done

--- src/components/Country.js
+++ src/components/Country.js
@@ -22,16 +22,26 @@ const Country = ({ country }) => {
   if (!weatherData) return <CountryInfo country={country} />
   else
     return (
-      <div>
+      <div className="container">
         <CountryInfo country={country} />
-        <h1>{country.capital} weather report</h1>
-        <p>Temperature is {weatherData.main.temp.toFixed(0)} degrees Celcius</p>
-        <p>Wind Speed is {weatherData.wind.speed} mph </p>
-        <p> Weather: {weatherData.weather[0].main}</p>
-        <img
-          src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-          alt="pciture of weather icon"
-        />
+        <div className="card lg:card-side bg-blue-800 hover:bg-blue-700 shadow-xl max-w-screen-sm ">
+          <figure>
+            <img
+              src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+              alt="pciture of weather icon"
+            />
+          </figure>
+          <div className="card-body">
+            <h1 className="text-2xl">{country.capital} weather report</h1>
+            <p className="text-2xl">
+              Temperature : {weatherData.main.temp.toFixed(0)} degrees Celcius
+            </p>
+            <p className="text-2xl">
+              Wind Speed : {weatherData.wind.speed} mph{' '}
+            </p>
+            <p className="text-2xl"> Weather: {weatherData.weather[0].main}</p>
+          </div>
+        </div>
       </div>
     )
 }
--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -5,28 +5,28 @@ import { FaMapMarkedAlt } from 'react-icons/fa'
 
 const CountryInfo = ({ country }) => {
   return (
-    <div className="card my-20 bg-base-100 shadow-xl image-full h-auto max-w-md ">
+    <div className="card my-20 bg-base-100 shadow-xl max-w-screen-sm image-full ">
       <figure>
         <img className="opacity-70" src={country.flag} alt="Country flag" />
       </figure>
       <div className="card-body flex flex-col gap-6   ">
         <h1 className="card-title">{country.name}</h1>
         <p className="text-2xl hover:text-red-900">
-          <GiCapitol className="inline pr-4 text-3xl" /> {country.capital}
+          <GiCapitol className="inline pr-4 text-4xl" /> {country.capital}
         </p>
         <p className=" text-2xl hover:text-red-900">
-          <MdEmojiPeople className="inline pr-4 text-3xl" />
+          <MdEmojiPeople className="inline pr-4 text-4xl" />
           {country.population}
         </p>
 
         <p className=" text-2xl tracking-wide hover:text-red-900">
-          <FaMapMarkedAlt className="inline pr-4 text-3xl" />
-          {country.subregion} located in {country.region}
+          <FaMapMarkedAlt className="inline pr-4 text-4xl" />
+          {country.subregion}
         </p>
 
         {country.currencies.map((currency) => (
           <p className=" text-2xl hover:text-red-900" key={currency.code}>
-            <GiMoneyStack className="inline pr-4 text-3xl" />
+            <GiMoneyStack className="inline pr-4 text-4xl" />
             {currency.name} ({currency.symbol})
           </p>
         ))}
